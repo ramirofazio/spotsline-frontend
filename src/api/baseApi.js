f32d3de545abe26645eafa102cf194d7415f0794
Ramiro <ramifazio@gmail.com>
2023-12-11 17:44:30 -0300

    fix base api

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const BASE_API = axios.create({
   maxBodyLength: 750000000,
   baseURL: API_URL,
-  headers: {
-    // ? Configuracion las cabeceras CORS
-    "Access-Control-Allow-Origin": import.meta.env.VITE_ACCESS_URL,
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Origin, Content-Type, Accept, Authorization",
-  },
 });
