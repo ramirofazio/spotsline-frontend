3e0aaeb23f7f39d2a378e5b5286e6a8accf358ca
TomasPerez1 <tomasperez.henry@gmail.com>
2023-12-14 15:08:26 -0300

    add NextUI responsive nav

--- src/components/navs/ModalNav.jsx
+++ src/components/navs/ModalNav.jsx
@@ -3,11 +3,8 @@ import { links } from ".";
 import React from "react";
 import {
   Navbar,
-  NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Button,
-  NavbarMenuItem,
   NavbarMenuToggle,
   NavbarMenu,
 } from "@nextui-org/react";
@@ -30,8 +27,7 @@ export function ModalNav() {
   ];
 
   return (
-    <Navbar className="flex items-center " onMenuOpenChange={setIsMenuOpen}>
-
+    <Navbar className="flex items-center  w-fit" onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent className="hidden gap-4 lg:flex " justify="start">
         {links.map((l, i) => (
           <NavbarItem className="0">
@@ -50,41 +46,32 @@ export function ModalNav() {
           </NavbarItem>
         ))}
       </NavbarContent>
-      <NavbarContent justify="center" className="flex gap-10 border-2 justify-items-end">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="lg:hidden border-2" />
+      <NavbarContent justify="center" className="flex gap-10 w-fit ">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="lg:hidden justify-self-end" />
         <i
           className="ri-instagram-line  icons  text-3xl text-yellow "
           onClick={() => window.open("https://www.instagram.com/spotsline_iluminacion/?hl=es-la", "_blank")}
         />
       </NavbarContent>
       <NavbarMenu>
-        {links.map((l) => (
-          <NavbarMenuItem >
-            <Link className="capitalize" to={l.path}>
+      {links.map((l, i) => (
+          <NavbarItem className="0">
+            <NavLink
+              key={i}
+              to={l.path}
+              className={({ isActive }) =>
+                `group mx-4 flex items-center text-lg uppercase tracking-wider transition  ${
+                  isActive ? "text-yellow" : "text-gray"
+                }`
+              }
+            >
+              <i className="ri-arrow-down-s-line mr-1 text-xl text-yellow transition group-hover:text-gray" />
               {l.name}
-            </Link>
-          </NavbarMenuItem>
+            </NavLink>
+          </NavbarItem>
         ))}
       </NavbarMenu>
     </Navbar>
   );
 }
 
-/* export function ModalNav() {
-  return( 
-    <Dropdown className="w-[200%]">
-      <DropdownTrigger>
-        <i className="ri-menu-fill text-3xl">
-        </i>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions" className="w-full border-2">
-        <DropdownItem className="" key="new">New file</DropdownItem>
-        <DropdownItem key="copy">Copy link</DropdownItem>
-        <DropdownItem key="edit">Edit file</DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
-} */
--- src/components/navs/NavBar.jsx
+++ src/components/navs/NavBar.jsx
@@ -1,5 +1,4 @@
-import { NavLink } from "react-router-dom";
-import { links, SearchInput, ModalNav } from "./index";
+import { SearchInput, ModalNav } from "./index";
 import logo from "assets/logo.png";
 import { ButtonWithIcon } from "components/buttons";
 
@@ -8,31 +7,11 @@ export function NavBar() {
     <main className="flex w-full items-center justify-between  bg-white  p-6">
       <img src={logo} className="w-40" />
       <div className="flex w-full flex-col items-end gap-4 p-2">
-        <section className="flex items-center space-x-4">
+        <section className="flex items-center space-x-4 mb-6">
           <SearchInput />
           <ButtonWithIcon icon={"ri-user-fill"} text={"INGRESA"} classname={"space-x-4 rounded-full"} />
         </section>
         <ModalNav />
-        {/* <nav className="hidden items-center border-2 lg:flex ">
-          {links.map((l, index) => (
-            <NavLink
-              key={index}
-              to={l.path}
-              className={({ isActive }) =>
-                `group mx-4 flex items-center text-lg uppercase tracking-wider transition  ${
-                  isActive ? "text-yellow" : "text-gray"
-                }`
-              }
-            >
-              <i className="ri-arrow-down-s-line mr-1 text-xl text-yellow transition group-hover:text-gray" />
-              {l.name}
-            </NavLink>
-          ))}
-          <i
-            className="ri-instagram-line icons ml-20 text-3xl text-yellow"
-            onClick={() => window.open("https://www.instagram.com/spotsline_iluminacion/?hl=es-la", "_blank")}
-          />
-        </nav> */}
       </div>
     </main>
   );
