ccb0fdfdd86abfe339b8556c0f90e1b4e351a6c7
ramirofazio <ramifazio@gmail.com>
2024-02-06 17:13:54 -0300

    fix

--- src/pages/routes/PublicRoutes.jsx
+++ src/pages/routes/PublicRoutes.jsx
@@ -9,6 +9,10 @@ const Landing = lazy(() => import("pages/landing/Landing.jsx").then((module) => 
 const AboutUs = lazy(() => import("pages/aboutUs/AboutUs.jsx").then((module) => ({ default: module.AboutUs })));
 const Company = lazy(() => import("pages/company/Company.jsx").then((module) => ({ default: module.Company })));
 const Products = lazy(() => import("pages/products/Products.jsx").then((module) => ({ default: module.Products })));
+const SignIn = lazy(() => import("pages/signIn/SignIn").then((module) => ({ default: module.SignIn })));
+const ChangePassword = lazy(() =>
+  import("pages/ChangePassword/ChangePassword").then((module) => ({ default: module.ChangePassword }))
+);
 
 export const publicRoutesPaths = [
   {
