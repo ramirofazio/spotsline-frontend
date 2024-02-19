48c33a46351133d1691d1461264377e2367448fd
ramirofazio <ramifazio@gmail.com>
2024-02-19 15:18:32 -0300

    add toasts

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { NextUIProvider } from "@nextui-org/react";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 import "swiper/css";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <NextUIProvider>
         <main className="bg-background text-foreground light">
+          <Toaster duration={3000} style={{ backgroundColor: "#D9D9D9" }} richColors={true} />
           <Routes />
         </main>
       </NextUIProvider>
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -3,11 +3,18 @@ import FirstSection from "./FirstSection";
 import CategoriesCarrousel from "./CategoriesCarrousel";
 import FeaturedProducts from "./FeaturedProducts";
 import SecondSection from "./SecondSection";
+import { toast } from "sonner";
 
 export function Landing() {
   useEffect(() => {
     document.title = "SPOTSLINE - Iluminación Profesional";
     //? Una boludez, pero queda lindo y mas dinamico
+
+    toast("Ya tenemos toasts!", {
+      description: "Estan buenos, usemoslos. Ej en Landing.jsx ;)",
+      duration: 5000,
+      icon: <i class="ri-thumb-up-line"></i>,
+    });
   }, []);
 
   return (
