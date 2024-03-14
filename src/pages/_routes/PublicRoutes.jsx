47faa706da6825995c435cd5284fb20fd300ab39
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2024-03-14 12:56:06 -0300

    un comment AboutUs

--- src/pages/_routes/PublicRoutes.jsx
+++ src/pages/_routes/PublicRoutes.jsx
@@ -8,7 +8,7 @@ import { DetailProduct } from "../products/detail/DetailProduct";
 const Footer = lazy(() => import("components/navs/Footer.jsx"));
 const NavBar = lazy(() => import("components/navs/NavBar.jsx"));
 const Landing = lazy(() => import("pages/landing/Landing.jsx").then((module) => ({ default: module.Landing })));
-//const AboutUs = lazy(() => import("pages/aboutUs/AboutUs.jsx").then((module) => ({ default: module.AboutUs })));
+const AboutUs = lazy(() => import("pages/aboutUs/AboutUs.jsx").then((module) => ({ default: module.AboutUs })));
 const Company = lazy(() => import("pages/company/Company.jsx").then((module) => ({ default: module.Company })));
 const Products = lazy(() => import("pages/products/Products.jsx").then((module) => ({ default: module.Products })));
 const SignIn = lazy(() => import("pages/signIn/SignIn").then((module) => ({ default: module.SignIn })));
@@ -48,7 +48,7 @@ export const publicRoutesPaths = [
       },
       { path: "/empresa", element: <Company /> },
       { path: "/rrhh", element: <RecursosHumanos /> },
-      // { path: "/contacto", element: <AboutUs /> },
+      { path: "/contacto", element: <AboutUs /> },
       { path: "/change-password", element: <ChangePassword /> },
       {
         path: "/productos/:page",
