59edf4220c8c6027ee757d11614c964363e71411
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2024-03-20 18:45:28 -0300

    update

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,7 @@ const route = {
   USER: "users",
   CART: "shoppingCart",
   CHECKOUT: "mobbex",
+  CUPON: "cupon"
 };
 
 export const APISpot = {
@@ -57,7 +58,7 @@ export const APISpot = {
   },
   cart: {
     validateCoupon: async (coupon) => {
-      const res = await BASE_API.get(`/${route.CART}/validate-coupon`, { coupon });
+      const res = await BASE_API.get(`/${route.CUPON}/validate/${coupon}`, );
       return res.data;
     },
   },
