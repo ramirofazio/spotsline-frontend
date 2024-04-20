f3fdccd5ce4273b4e8ef33e6c9c8941d95ee9b59
tomasPerez <tomasperez.developer@gmail.com>
2024-04-20 12:44:23 -0300

    add web_orders

--- src/pages/dashboard/Orders.jsx
+++ src/pages/dashboard/Orders.jsx
@@ -1,72 +1,96 @@
 import React, { useCallback } from "react";
 import { useLoaderData } from "react-router-dom";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+
+import { convertISOToDate, copyToClipboard } from "src/utils";
 
 export function Orders() {
   const orders = useLoaderData();
 
+  const orders_columns = [
+    { label: "Codigo", key: "id" },
+    { label: "Email", key: "email" },
+    { label: "Nombre", key: "name" },
+    { label: "Total", key: "total" },
+    { label: "Subtotal", key: "subtotal" },
+    { label: "Descuento", key: "discount" },
+    { label: "Fecha de emisión", key: "date" },
+  ];
+
   const renderCell = useCallback((item, columnKey) => {
     const cellValue = item[columnKey];
-
     switch (columnKey) {
-      case "cuit":
-        const color = item.email ? "bg-green-500" : "bg-red-500";
+      case "id":
         return (
-          <div className="flex items-center gap-2">
-            <Tooltip
-              color="primary"
-              delay={200}
-              content={color ? "Este cliente NO tiene un Email valido" : "Este cliente tiene un Email valido"}
-            >
-              <div className={`h-2 w-2 rounded-full shadow-xl ${color}`} />
-            </Tooltip>
-            <p className="bg-gradient-to-r from-dark to-yellow-700 bg-clip-text font-bold text-transparent">
+          <span className="flex min-w-[200px] items-center  gap-1 ">
+            <p className=" w-full bg-gradient-to-r  from-dark to-yellow-700 bg-clip-text font-bold text-transparent">
               {cellValue}
             </p>
-          </div>
+            <i
+              className="ri-file-copy-line icons  bg-gradient-to-r from-primary to-yellow-600 bg-clip-text text-xl text-transparent"
+              onClick={() => copyToClipboard(cellValue)}
+            />
+          </span>
         );
       case "email":
-        if (cellValue) {
-          return <p className="uppercase">{cellValue.split(";")[0]}</p>;
-        } else {
-          return (
-            <Tooltip content={"Agregar Email a este cliente"} delay={1000} color="primary">
-              <div
-                className="icons w-full rounded-md border-2 border-primary/80 text-center"
-                onClick={() => handleModal(item)}
-              >
-                <i className={`ri-add-line text-xl font-bold`} />
-              </div>
-            </Tooltip>
-          );
-        }
+        return <p className="uppercase">{cellValue}</p>;
+      case "name":
+        return <p className="min min-w-[150px] uppercase">{cellValue}</p>;
+      case "total":
+        return <p className="uppercase">${cellValue}</p>;
+      case "subtotal":
+        return <p className="uppercase">${cellValue}</p>;
 
-      case "active":
+      case "discount":
         return (
-          <div className="flex justify-center">
-            <Tooltip content={cellValue ? "Cliente activo" : "Cliente Inactivo"} delay={1000} color="primary">
-              <i
-                className={`${
-                  cellValue
-                    ? "ri-check-fill bg-gradient-to-r from-green-600 to-yellow-600 bg-clip-text text-transparent"
-                    : "ri-close-line bg-gradient-to-r from-dark to-yellow-600 bg-clip-text text-transparent"
-                } icons text-center text-xl font-bold`}
-              />
-            </Tooltip>
-          </div>
+          <p className={`uppercase ${cellValue ? "text-md text-green-600" : "text-xs text-red-600"} `}>
+            {cellValue ? `%${cellValue}` : "no dispone"}
+          </p>
         );
-      case "priceList":
-        return <p className="text-center font-bold">{cellValue}</p>;
+
+      case "date":
+        return <p className="">{convertISOToDate(cellValue)}</p>;
 
       default:
         return cellValue;
     }
   }, []);
 
+  const renderCol = useCallback((key, label) => {
+    switch (key) {
+      default:
+        return <p>{label}</p>;
+    }
+  }, []);
+
   return (
-    <div>
-      {orders.map((o) => (
-        <p key={o.id}>{o.id}</p>
-      ))}
-    </div>
+    <main className="flex flex-col items-center">
+      <Table
+        aria-label="Example table with custom cells"
+        isStriped
+        removeWrapper
+        isHeaderSticky
+        className="!z-20"
+        classNames={{
+          th: "bg-gradient-to-b from-primary to-yellow-600",
+          base: "overflow-y-scroll rounded-md min-h-[600px] max-h-[600px] backdrop-blur-sm",
+        }}
+      >
+        <TableHeader columns={orders_columns}>
+          {(column) => (
+            <TableColumn key={column.key} className="text-sm uppercase !text-dark">
+              {renderCol(column.key, column.label)}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={orders}>
+          {(item) => (
+            <TableRow key={item.codigo}>
+              {(columnKey) => <TableCell className="relative font-medium">{renderCell(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </main>
   );
 }
