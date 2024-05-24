82718dd9a59e03888b32428abb2589c60f392a7e
ramiro <ramifazio@gmail.com>
2024-05-24 13:30:49 -0300

    fix

--- src/components/images/AwsImage.jsx
+++ src/components/images/AwsImage.jsx
@@ -1,13 +1,14 @@
+import { Image } from "@nextui-org/react";
 import { assets } from "src/assets/index";
 
 export default function AwsImage({ type, identify, className, ...props }) {
   return (
-    <img
-      loading="eager"
+    <Image
+      loading="lazy"
       radius="none"
       src={assets[type][identify]}
-      width={200}
-      height={200}
+      width={0}
+      height={0}
       alt={identify}
       className={`${className} pointer-events-none max-w-[500px]`}
       {...props}
