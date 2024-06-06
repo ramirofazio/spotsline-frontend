be7e3f953aee697420947176bad13283a516c3a3
ramiro <ramifazio@gmail.com>
2024-06-06 15:39:26 -0300

    add smooth scrolling

--- src/components/ExternalTags.jsx
+++ src/components/ExternalTags.jsx
@@ -1,6 +1,17 @@
 import { Analytics } from "@vercel/analytics/react";
+import Lenis from "lenis";
 
 export default function ExternalTags() {
+  //? Smooth scrolling
+  const lenis = new Lenis({ lerp: 0.06 });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
   return (
     <>
       <Analytics />
